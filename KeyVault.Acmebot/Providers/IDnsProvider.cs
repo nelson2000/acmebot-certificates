65b3b68935733c718690656420ddbd8aa42749e4
Nelson Nwajie <nwajienelson@gmail.com>
2023-06-24 10:09:29 -0400

    new commit

---
+++ KeyVault.Acmebot/Providers/IDnsProvider.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace KeyVault.Acmebot.Providers;
+
+public interface IDnsProvider
+{
+    int PropagationSeconds { get; }
+    Task<IReadOnlyList<DnsZone>> ListZonesAsync();
+    Task CreateTxtRecordAsync(DnsZone zone, string relativeRecordName, IEnumerable<string> values);
+    Task DeleteTxtRecordAsync(DnsZone zone, string relativeRecordName);
+}
